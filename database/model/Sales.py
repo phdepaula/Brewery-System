d11b83ad21156ada78964f050bfcd357e32a9151
phdepaula <pedrohenriquedepaulaalves@yahoo.com.br>
2023-10-31 21:14:05 -0300

    feat: New table Sales

---
+++ database/model/Sales.py
@@ -0,0 +1,37 @@
+from sqlalchemy import (
+    Column, DateTime, Float, ForeignKey, Integer, String, func
+)
+from sqlalchemy.orm import relationship
+
+from database.Settings import database
+
+
+class Sales(database.BASE):
+    """Class to create the sales table"""
+
+    __tablename__ = "sales"
+
+    id = Column("sales_id", Integer, primary_key=True, autoincrement=True)
+    name = Column(String(255), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    price = Column(Float, nullable=False)
+    date = Column(DateTime, default=func.current_timestamp())
+    beer_id = Column(Integer, ForeignKey("beer.beer_id"), nullable=False)
+    client_id = Column(Integer, ForeignKey("client.client_id"), nullable=False)
+
+    beer = relationship("Beer", back_populates="sales")
+    client = relationship("Client", back_populates="sales")
+
+    def __init__(
+        self,
+        name: str,
+        quantity: int,
+        price: float,
+        beer_id: int,
+        client_id: int
+    ):
+        self.name = name
+        self.quantity = quantity
+        self.price = float("{:.2%}".format(price))
+        self.beer_id = beer_id
+        self.client_id = client_id
