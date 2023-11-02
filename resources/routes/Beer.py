b31821fa0be59f669bf9db6f985491a0ece3a2c5
phdepaula <pedrohenriquedepaulaalves@yahoo.com.br>
2023-11-02 17:21:39 -0300

    fix: Fixing tag initialization

---
+++ resources/routes/Beer.py
@@ -0,0 +1,43 @@
+from typing import Any, Dict, Union
+
+from fastapi import HTTPException
+
+from database.model.Beer import Beer
+from resources.schemas.Beer import (GetBeerSchema, MessageSchema,
+                                    format_beer_data)
+from resources.settings.App import app
+from resources.util.TableHandler import TableHandler
+
+TABLE_HANDLER = TableHandler()
+
+
+@app.get(
+    "/get_beer/{name}",
+    tags=["Beer"],
+    response_model=GetBeerSchema,
+    responses={
+        200: {"model": GetBeerSchema},
+        400: {"model": MessageSchema},
+    },
+)
+def get_beer(name: str) -> Union[
+    Dict[str, str], Dict[str, Union[str, Dict[str, Any]]]
+]:
+    """Route to get all the data for a specific beer."""
+    try:
+        beer_data = TABLE_HANDLER.select_data_table(
+            table=Beer, filter_select={Beer.name: name}
+        )
+        quantity_beer = len(beer_data)
+
+        if quantity_beer > 0:
+            message = f"The beer {name} was found."
+            beer_data_json = format_beer_data(beer_data[0])
+        else:
+            message = f"The beer {name} was not found."
+            beer_data_json = {}
+
+        response = {"message": message, "beer": beer_data_json}
+        return response
+    except Exception as error:
+        raise HTTPException(status_code=400, detail=str(error))
--- resources/routes/Documentation.py
+++ resources/routes/Documentation.py
@@ -1,15 +1,9 @@
-from fastapi.openapi.models import Tag
 from fastapi.responses import RedirectResponse
 
 from resources.settings.App import app
 
-TAG_DOCUMENTATION = Tag(
-    name="Documentation",
-    description="Documentation selection: Swagger.",
-)
 
-
-@app.get("/", tags=[TAG_DOCUMENTATION])
+@app.get("/", tags=["Documentation"])
 def documentation_route():
     """
     Redirects to the /swagger route,\
