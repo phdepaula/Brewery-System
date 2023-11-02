47296a9bceeb79737395915d429c1782d70b9478
phdepaula <pedrohenriquedepaulaalves@yahoo.com.br>
2023-11-02 14:38:45 -0300

    feat: Creating fast api app

---
+++ resources/settings/App.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+
+class App(FastAPI):
+    "Class to generate fast api app."
+
+    API_TITLE = "Brewery System API"
+    VERSION = "1.0.0"
+    DESCRIPTION = "API's created to manage the sales of a brewery."
+    OPEN_API_JSON_URL = "/swagger/openapi.json"
+    SWAGGER_URL = "/swagger"
+
+    def __init__(self):
+        super().__init__(
+            title=self.API_TITLE,
+            description=self.DESCRIPTION,
+            version=self.VERSION,
+            openapi_url=self.OPEN_API_JSON_URL,
+            docs_url=self.SWAGGER_URL,
+        )
+
+
+app = App()
